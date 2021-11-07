9607dad1befd3a5f30392a9d777305ddc1453010
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-11-07 14:24:17 +0200

    Initial Commit

---
+++ 24242/_4242App.swift
@@ -0,0 +1,17 @@
+//
+//  _4242App.swift
+//  24242
+//
+//  Created by Vahtee Boo on 07.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct _4242App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
